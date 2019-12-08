7473958b6f89928ed577507333c3dfda33b4686e
abdikeem <abdihakimabdirahman21@gmail.com>
2019-12-08 13:09:13 +0300

    add date-count-pipe

--- Git-search/src/app/app.module.ts
+++ Git-search/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { UserService } from './user.service';
-
+import { DateCountPipe } from './date-count-pipe'
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
@@ -15,6 +15,7 @@ import { RepoComponent } from './repo/repo.component';
     AppComponent,
     UserComponent,
     RepoComponent,
+    DateCountPipe
     
   ],
   imports: [
